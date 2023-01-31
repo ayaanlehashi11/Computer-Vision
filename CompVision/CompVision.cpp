6b7e0f3602f8c728dc25dcb07bc2e455517279c4
ayaanle <ayaanlemohamed540@gmail.com>
2023-01-31 17:27:15 +0300

    CompVision

--- CompVision/CompVision.cpp
+++ CompVision/CompVision.cpp
@@ -1,31 +1,27 @@
 #include <wx/wx.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video/background_segm.hpp>
+#include <opencv2/features2d.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
 #include <wx/slider.h>
+#include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
 using namespace cv;
+
+
 class Frame : public wxFrame
 {
 public:
     Frame(const wxString& title)
 		: wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(650, 570))
 	{
-
-	slider->SetForegroundColour(wxColour(*wxGREEN));
 	text->SetForegroundColour(wxColour(*wxGREEN));
 	text->SetDefaultStyle(wxTextAttr(*wxGREEN));
-	slider->Bind(wxEVT_SLIDER,[&](wxCommandEvent& event)
-	{
-	    cout << "the Current Slider value is: " << slider->GetValue() << endl;
-	});
-	/**this button helps you to exit and finish the whole program
-	*/
-	halt->Bind(wxEVT_BUTTON,[&](wxCommandEvent& event){
-	    exit(1);
-	});
 	button2->Bind(wxEVT_BUTTON,[&](wxCommandEvent& event){
 	text->SetValue("Detecting the face of the user...");
 	cout << "Detecting the face of the user..." << endl;
@@ -58,7 +54,7 @@ public:
 	});
          //basic video recorder
 	        button1->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event){
-	            cout << "Ayaanle is here baby..." << endl;
+	            cout << "Video recording without Detection..." << endl;
 	            Mat image;
         VideoCapture cap(0);
         if (!cap.isOpened()){
@@ -83,47 +79,16 @@ public:
 	        });
 		Centre();
 	}
-	void HandDetection(Mat& img , CascadeClassifier* cascade , double scale);
+
        
 public:  
     wxPanel* panel = new wxPanel(this);
     wxTextCtrl* text = new wxTextCtrl(panel , wxID_ANY,wxEmptyString , {100 , 170},wxSize(440 , 100),wxTE_MULTILINE);
-    wxButton* button1 = new wxButton(panel , wxID_ANY, "grey",{316 , 10});
-    wxButton* button2 = new wxButton(panel , wxID_ANY, "detect",{130 , 10});
-    wxButton* button3 = new wxButton(panel , wxID_ANY, "click", {40 , 10});
-    wxButton* halt = new wxButton(panel , wxID_ANY,"halt",{224 , 10});
-    wxSlider* slider = new wxSlider(panel , wxID_ANY , 16, 0 , 100,{400 , 100},wxSize(200 , -1),wxSL_HORIZONTAL|wxSL_AUTOTICKS|wxSL_LABELS);
+    wxButton* button1 = new wxButton(panel , wxID_ANY, "detect",{130 , 10});
+    wxButton* button2 = new wxButton(panel , wxID_ANY, "record", {40 , 10});
         wxTextCtrl* text1 = new wxTextCtrl(panel , wxID_ANY,wxEmptyString , {100 , 400},wxSize(440 , 100),wxTE_MULTILINE);
     
 };
-
-
-
-/**
-Mat stream;
-	            VideoCapture cap(0);
-	            namedWindow("Face Detection");
-	            string classifier_path = "/home/ayaanle/opencv-build/opencv/data/haarcascade/haarcascade_frontalcatface.xml";
-	            CascadeClassifier faceDetector;
-	            faceDetector.load(classifier_path);
-
-	            vector<Rect>faces;
-	            while(true){
-	               faceDetector.detectMultiScale(stream , faces , 1.1 , 1 , 0 |CASCADE_SCALE_IMAGE,Size(30 , 30));
-	               cap.read(stream);
-	               for(int i = 0; i < faces.size();++i){
-	                  Mat faceROI = stream(faces[i]);
-	                  int x = faces[i].x;
-	                  int y = faces[i].y;
-	                  int h = y + faces[i].height;
-	                  int w = x + faces[i].width;
-	                  rectangle(stream , Point(x , y),Point(w , h),Scalar(255 , 0 , 255),2 , 8 , 0);
-	               }
-	               imshow("Face Detection" , stream);
-	               if(waitKey(10) == 27)
-	                  break;
-	            }
-*/
 
 
 
